147ec609d51fa847175ec5b360e7920fe5b07e8a
sezasada <111367099+sezasada@users.noreply.github.com>
2022-12-29 16:33:16 -0800

    update js
    added some guidance

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ console.log('hello');
 
 function displayRevenue() {
     // Placeholder value for revenue
-    let revenue = 1000;
-    let peRatio = 1.3;
+    let revenue = '90.15B';
+    let peRatio = 21.5358;
   
     // Create a new table row
     let row = document.createElement("tr");
@@ -25,7 +25,7 @@ function displayRevenue() {
     row.appendChild(peRatioCell);
 
     let guidanceCell = document.createElement("td");
-    guidanceCell.innerHTML = "This is what we have to do jkh fa hja hjhasjfhdjsahfhsa fhsadh fkjsadhjkfh sadkj";
+    guidanceCell.innerHTML = "This quarter’s results reflect Apple’s commitment to our customers, to the pursuit of innovation, and to leaving the world better than we found it. As we head into the holiday season with our most powerful lineup ever, we are leading with our values in every action we take and every decision we make. We are deeply committed to protecting the environment, to securing user privacy, to strengthening accessibility, and to creating products and services that can unlock humanity’s full creative potential. Our record September quarter results continue to demonstrate our ability to execute effectively in spite of a challenging and volatile macroeconomic backdrop. We continued to invest in our long-term growth plans, generated over $24 billion in operating cash flow, and returned over $29 billion to our shareholders during the quarter. The strength of our ecosystem, unmatched customer loyalty, and record sales spurred our active installed base of devices to a new all-time high. This quarter capped another record-breaking year for Apple, with revenue growing over $28 billion and operating cash flow up $18 billion versus last year.";
     row.appendChild(guidanceCell);
   
     // Add the new row to the table
