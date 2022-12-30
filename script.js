2aa28df649c5974b02e5e639712f9f8362687602
sezasada <111367099+sezasada@users.noreply.github.com>
2022-12-29 16:21:46 -0800

    script.js
    Changed the function a lot. added new variables. Not done yet.

--- script.js
+++ script.js
@@ -1,53 +1,38 @@
 console.log('hello');
 
-$(function () {
-    console.log('js working');
-    appendDom(); 
-    $('#submitButton').on("click", decFunction);
-});
 
-const stock = [];
 
-function addToCollection (peRatio, revenue, guidance, chart) {
-    const newObject = {
-        peRatio: peRatio,
-        revenue: revenue,
-        guidance: guidance,
-        chart: chart
-    }
-    stock.push(newObject);
-    return true;
-}
-console.log(stock);
-console.log(addToCollection('30', '34', 'do this', 'chart'));
+function displayRevenue() {
+    // Placeholder value for revenue
+    let revenue = 1000;
+    let peRatio = 1.3;
+  
+    // Create a new table row
+    let row = document.createElement("tr");
+  
+    // Create the first cell for the company name
+    let companyCell = document.createElement("td");
+    companyCell.innerHTML = "Apple";
+    row.appendChild(companyCell);
+  
+    // Create the second cell for the revenue
+    let revenueCell = document.createElement("td");
+    revenueCell.innerHTML = "$" + revenue;
+    row.appendChild(revenueCell);
 
-function appendDom () {
-    $("#tableBody").empty();
-    for( let i = 0; i < stock.length; i++) {
-        const element = stock[i];
+    let peRatioCell = document.createElement("td");
+    peRatioCell.innerHTML = peRatio;
+    row.appendChild(peRatioCell);
 
-        $("#tableBody").append(`<tr>
-          <td>${element.peRatio}</td>
-          <td>${element.revenue}</td>
-          <td>${element.guidance}</td>
-          <td>${element.chart}</td>
-        </tr>`);
-    }
-}
-
-function decFunction (){
-    let a = $("#peRatio").val();
-    let b = $("#revenue").val();
-    let c = $("#guidance").val();
-    let d = $("#chart").val();
-
-    addToCollection(a, b, c, d);
-
-    $('#peRatio'). val('');
-    $('#revenue'). val('');
-    $('#guidance'). val('');
-    $('#chart'). val('');
-
- appendDom();
-
-}
+    let guidanceCell = document.createElement("td");
+    guidanceCell.innerHTML = "This is what we have to do jkh fa hja hjhasjfhdjsahfhsa fhsadh fkjsadhjkfh sadkj";
+    row.appendChild(guidanceCell);
+  
+    // Add the new row to the table
+    let table = document.getElementById("revenue-table");
+    table.appendChild(row);
+  }
+  
+  // Attach the displayRevenue function to the button's click event
+  let button = document.getElementById("button");
+  button.addEventListener("click", displayRevenue);
